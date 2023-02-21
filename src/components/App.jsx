d99a3fe49184373114835bc672a2e4fc65693a33
Dina-Ursula <dinatyan26@gmail.com>
2023-02-21 12:30:45 +0200

    add/proptypes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,15 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import TodoList from './TodosList/TodosList';
 
 class App extends Component {
   state = {
     todos: [
-      { id: 3, text: 'Learn', completed: false },
-      { id: 4, text: 'Write', completed: false },
-      { id: 5, text: 'Move', completed: false },
+      { id: 3, text: 'Learn', completed: true },
+      { id: 4, text: 'Write', completed: true },
+      { id: 5, text: 'Make homework', completed: false },
+      { id: 6, text: 'Search in Google', completed: true },
+      { id: 7, text: 'Improve Skils', completed: false },
+      { id: 8, text: 'Find first job', completed: false },
     ],
   };
   deleteTodo = todoId => {
@@ -17,8 +19,19 @@ class App extends Component {
   };
   render() {
     const { todos } = this.state;
+
+    const totalTodoCount = todos.length;
+    const completedTodosCount = todos.reduce(
+      (acc, todo) => (todo.completed ? acc + 1 : acc),
+      0
+    );
+
     return (
       <>
+        <div>
+          <p>Total amount:{totalTodoCount}</p>
+          <p>Amount done:{completedTodosCount}</p>
+        </div>
         <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
       </>
     );
--- src/components/TodosList/TodosList.jsx
+++ src/components/TodosList/TodosList.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TodoList = ({ todos, onDeleteTodo }) => (
   <ul>
     {todos.map(({ id, text }) => (
       <li key={id}>
         <p>{text}</p>
-        <button onClick={() => onDeleteTodo(id)}>deleted</button>
+        <button onClick={() => onDeleteTodo(id)}>delete</button>
       </li>
     ))}
   </ul>
 );
 export default TodoList;
+
+TodoList.propTypes = {
+  todos: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
