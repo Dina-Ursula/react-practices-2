9117da5ecd17ae5b9820ed66bd3f5e529c8690f9
Dina-Ursula <dinatyan26@gmail.com>
2023-02-21 11:25:24 +0200

    feature/button-delete

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import TodoList from './TodosList/TodosList';
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 3, text: 'Learn', completed: false },
+      { id: 4, text: 'Write', completed: false },
+      { id: 5, text: 'Move', completed: false },
+    ],
+  };
+  deleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todo.id !== todoId),
+    }));
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <>
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+      </>
+    );
+  }
+}
+export default App;
---
+++ src/components/TodosList/TodosList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoList = ({ todos, onDeleteTodo }) => (
+  <ul>
+    {todos.map(({ id, text }) => (
+      <li key={id}>
+        <p>{text}</p>
+        <button onClick={() => onDeleteTodo(id)}>deleted</button>
+      </li>
+    ))}
+  </ul>
+);
+export default TodoList;
